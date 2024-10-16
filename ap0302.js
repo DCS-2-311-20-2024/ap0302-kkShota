63e7cf0562a3f1c5ebd40f2cf470673a228d05f0
片野　翔太 <124877346+G084342020@users.noreply.github.com>
2024-10-16 17:48:52 +0900

    第3回-2　TA
    車の作成

--- ap0302.js
+++ ap0302.js
@@ -1,6 +1,6 @@
 //
 // 応用プログラミング 第4回 課題2 (ap0302)
-// G184002021 拓殖太郎
+// G4M3042024 片野 翔太
 //
 "use strict"; // 厳格モード
 
@@ -11,10 +11,10 @@ import { myTriangleGeometry } from './myTriangleGeometry.js'
 // ３Ｄページ作成関数の定義
 function init() {
   const param = { // カメラの設定値
-    fov: 20, // 視野角
+    fov: 60, // 視野角
     x: 30,
     y: 10,
-    z: 30,
+    z: 40,
     wireframe: false
   };
 
@@ -24,7 +24,8 @@ function init() {
   // 座標軸の設定
   const axes = new THREE.AxesHelper(18);
   scene.add(axes);
-  
+  axes.visible = false;
+
   // 素材の設定
   const glassMaterial = new THREE.MeshPhongMaterial({color: 'skyblue'});
   const bodyMaterial = new THREE.MeshPhongMaterial({color: 'orangered'});
@@ -52,30 +53,68 @@ function init() {
   const car = new THREE.Group();
   let mesh;
   //   ボディの作成
-
+  mesh = new THREE.Mesh(new THREE.BoxGeometry(carW, carH, carL), bodyMaterial);
+  mesh.position.y = -0.7;
+  car.add(mesh);
+  //影
+  car.children.forEach((child) => {
+    child.castShadow = true;
+    child.receiveShadow = true;
+  });
   //   屋根の作成
-
+  mesh = new THREE.Mesh(new myTriangleGeometry( v[2], v[3], v[6]), bodyMaterial);
+  car.add(mesh);
+  mesh = new THREE.Mesh(new myTriangleGeometry( v[7], v[6], v[3]), bodyMaterial);
+  car.add(mesh);
   // 窓の作成
   //     左窓
   mesh = new THREE.Mesh(new myTriangleGeometry( v[0], v[1], v[2]), glassMaterial);
   car.add(mesh);
+  mesh = new THREE.Mesh(new myTriangleGeometry( v[3], v[2], v[1]), glassMaterial);
+  car.add(mesh);
   //     右窓
-
+  mesh = new THREE.Mesh(new myTriangleGeometry( v[5], v[7], v[4]), glassMaterial);
+  car.add(mesh);
+  mesh = new THREE.Mesh(new myTriangleGeometry( v[6], v[4], v[7]), glassMaterial);
+  car.add(mesh);
   //     前窓
-
+  mesh = new THREE.Mesh(new myTriangleGeometry( v[4], v[0], v[6]), glassMaterial);
+  car.add(mesh);
+  mesh = new THREE.Mesh(new myTriangleGeometry( v[2], v[6], v[0]), glassMaterial);
+  car.add(mesh);
   //     後窓
+  mesh = new THREE.Mesh(new myTriangleGeometry( v[1], v[3], v[5]), glassMaterial);
+  car.add(mesh);
+  mesh = new THREE.Mesh(new myTriangleGeometry( v[7], v[5], v[3]), glassMaterial);
+  car.add(mesh);
 
-  //   タイアの作成
+  //   タイヤの作成
   const tyreR = 0.8;
   const tyreW = 0.5;
   mesh = new THREE.Mesh(new THREE.CylinderGeometry(tyreR, tyreR, tyreW, 16, 1 ), tyreMaterial);
   mesh.rotation.z = Math.PI/2;
   mesh.position.set(carW/2, -carH, 3/8*carL);
   car.add(mesh);
+  mesh = new THREE.Mesh(new THREE.CylinderGeometry(tyreR, tyreR, tyreW, 16, 1 ), tyreMaterial);
+  mesh.rotation.z = Math.PI/2;
+  mesh.position.set(-carW/2, -carH, 3/8*carL);
+  car.add(mesh);
+  mesh = new THREE.Mesh(new THREE.CylinderGeometry(tyreR, tyreR, tyreW, 16, 1 ), tyreMaterial);
+  mesh.rotation.z = Math.PI/2;
+  mesh.position.set(carW/2, -carH, -3/8*carL);
+  car.add(mesh);
+  mesh = new THREE.Mesh(new THREE.CylinderGeometry(tyreR, tyreR, tyreW, 16, 1 ), tyreMaterial);
+  mesh.rotation.z = Math.PI/2;
+  mesh.position.set(-carW/2, -carH, -3/8*carL);
+  car.add(mesh);
   // 高さの調整
-  
+  car.position.y = carH + tyreR;
+  car.add(mesh);
   // 影の投影
-
+  car.children.forEach((child) => {
+    child.castShadow = true;
+    child.receiveShadow = true;
+  })
   scene.add(car);
 
   // 平面の設定
@@ -92,7 +131,9 @@ function init() {
   light1.position.set(0, 70, -3);
   light1.castShadow = true;
   scene.add(light1);
-    
+  const light2 = new THREE.AmbientLight('white', 0.5);
+  scene.add(light2);
+
   // カメラの設定
   const camera = new THREE.PerspectiveCamera(
     param.fov, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -114,10 +155,15 @@ function init() {
     camera.position.z = param.z;
     camera.lookAt(0, 0, 0);
     camera.updateProjectionMatrix();
+    theta = (theta + 0.01) % (2 * Math.PI);
+    car.position.x = radius * Math.cos(theta);
+    car.position.z = radius * Math.sin(theta);
+    car.rotation.y = -1*theta;
     car.children.forEach( (mesh) => {
       mesh.material.wireframe = param.wireframe;
     });
     renderer.render(scene, camera);
+    requestAnimationFrame(render);
   }
 
   // GUIコントローラ
--- myTriangleGeometry.js
+++ myTriangleGeometry.js
@@ -3,14 +3,14 @@
 //   応用プログラミング
 //
 import { 
-    BufferGeometry,
+    BufferGeometry, //BufferGeometryを継承
     Float32BufferAttribute,
     Vector3 } from 'three';
 
 export class myTriangleGeometry extends BufferGeometry {
     constructor(
         a = new Vector3(0,0,0),
-        b = new Vector3(1,0,0),
+        b = new Vector3(1,0,0), //3つ頂点をベクトルとして渡す
         c = new Vector3(0,1,0)) {
 
         super();
@@ -27,7 +27,7 @@ export class myTriangleGeometry extends BufferGeometry {
             (b-a).x * (c-a).y, (b-a).y * (c-a).x,
         )
         this.setAttribute('position',
-            new Float32BufferAttribute( vertices, 3));
+            new Float32BufferAttribute( vertices, 3)); //プロパティを設定する
         this.setAttribute('normal',
             new Float32BufferAttribute( vertices, 3));
     }
